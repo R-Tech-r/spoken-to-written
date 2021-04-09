09a6aa0109f106e5ba35b22af372bf7dd7d77551
R-Tech-r <67594050+R-Tech-r@users.noreply.github.com>
2021-04-10 00:54:46 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def open_file(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname))
+
+setup(
+	  name = 'spoken2written',
+      packages = ['spoken2written'],
+      version='0.1',
+      description='Convert Spoken English given as text to Written English ',
+      author='Rebika Devi',
+      author_email='devi72l@hotmail.com',
+      url='https://github.com/cyberdhiman/Spoken-To-Written-English',
+      classifiers = [
+     					 'Intended Audience :: Developers',
+      					'Programming Language :: Python'
+  				],
+	  long_description=open_file('README.rst').read()
+
+     )
